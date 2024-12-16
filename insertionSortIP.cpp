c594f446f24bd4d252de0ad9e83dbf9273f19d0a
PabloDurkheim <67520216+PabloDurkheim@users.noreply.github.com>
2024-12-16 13:28:29 -0300

    Update insertionSortIP.cpp

--- insertionSortIP.cpp
+++ insertionSortIP.cpp
@@ -3,11 +3,29 @@
 
 using namespace std;
 
+void imprimir(int *v, int tam){
+    
+    for(int i = 0; i < tam; i++){
+        cout << v[i] << " ";
+    }
+    
+    cout<<endl<<endl;
+    
+}
+
 //é como se fosse o bubble sort, só que para a esquerda
 void insertionSortIP(int *v, int tamanho){
    
     for (int i = 1; i < tamanho; i++) {   //i = indice da carta a ser inserida ordenada
+        
+        cout << "--- Iteração: " << i << " ---" <<endl<<endl;
+        
         for (int j = i; j > 0; j--) {
+
+            cout << "v["<<j-1<<"] > v["<<j<<"] ???"<<endl;
+            cout << v[j-1] << " > " << v[j] << "?"<<endl;
+            cout << "i: " << i << "     j: " << j << endl;
+            
             if (v[j - 1] > v[j]) {
                 int temp = v[j - 1];
                 v[j - 1] = v[j];
@@ -16,6 +34,8 @@ void insertionSortIP(int *v, int tamanho){
             else {
                 break;
             }
+            
+            imprimir(v, tamanho);
         }
     }
     
@@ -29,18 +49,16 @@ int main() {
     for(int i = 0; i < tamanho; i++){
         v[i] = rand() % 100;
     }
+
+    cout << "Vetor original: ";
     
     for(int i = 0; i < tamanho; i++){
         cout << v[i] << " ";
     }
     
-    cout << endl;
+    cout << endl<<endl;
     
     insertionSortIP(v, tamanho);
-    
-    for(int i = 0; i < tamanho; i++){
-        cout << v[i] << " ";
-    }
     
     return 0;
 }
